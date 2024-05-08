70d771bdd3387e1f35271ef8bb9870ab80b35f2f
Indi <indi@madmimic.com>
2024-05-08 17:21:05 -0300

    Good Ending Implementado

---
+++ Assets/Scripts/GoodEnding.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoodEnding : MonoBehaviour, IInteractable
+{
+    public bool IsGoodEnding;
+
+    public void Interact(PointClickInteraction pointClick)
+
+    {
+        if (IsGoodEnding)
+        {
+            SceneManager.LoadScene("MainMenu");
+
+        }
+
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public AnimationCurve hasCurve()
+    {
+        return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
